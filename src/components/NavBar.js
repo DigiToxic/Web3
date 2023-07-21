73f7ecd89001fb82801357414c092d9f584497e1
Digi | Toxic <DigiToxic55@gmail.com>
2023-07-21 17:39:25 -0400

    official

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import logo from "../Web3/src/assets/logo.png"
+import logo from "../assets/img/logo.svg";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 
